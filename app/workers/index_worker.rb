7cf5d4f8c23cb47e6f9cc7b15c687b7b48017f0e
Ryan Krage <krage.ryan@gmail.com>
2016-09-24 15:57:08 -0500

    only index document if record is persisted

--- app/workers/index_worker.rb
+++ app/workers/index_worker.rb
@@ -3,10 +3,18 @@ class IndexWorker
 
   sidekiq_options retry: false
 
+  attr_reader :resource
+
   def perform(id, klass, operation)
-    klass.constantize
-      .first_or_initialize_by_id(id)
-      .__elasticsearch__
-      .send(operation)
+    @resource = klass.constantize.first_or_initialize_by_id(id)
+    send(operation)
+  end
+
+  def index_document
+    resource.__elasticsearch__.index_document if resource.persisted?
+  end
+
+  def delete_document
+    resource.__elasticsearch__.delete_document
   end
 end
