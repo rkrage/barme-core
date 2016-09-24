f8f7d841fdfed52da7992d8373111a9abed165a6
Ryan Krage <krage.ryan@gmail.com>
2016-09-24 15:02:51 -0500

    reindex beers and their breweries on style update

--- app/models/beer_style.rb
+++ app/models/beer_style.rb
@@ -1,11 +1,14 @@
 class BeerStyle < ApplicationRecord
   lookup_for :beer_category
 
-  after_commit :index_beers, on: :update
+  after_commit :index_beers_and_breweries, on: :update
 
   has_many :beers, dependent: :destroy
 
-  def index_beers
-    beers.each(&:index_document)
+  def index_beers_and_breweries
+    beers.includes(:breweries).each do |beer|
+      beer.index_document
+      beer.breweries.each(&:index_document)
+    end
   end
 end
